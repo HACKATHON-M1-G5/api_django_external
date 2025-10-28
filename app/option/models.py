55f451c8e4a5e698a84f724506f85a6119416e7c
emile <emilepicard323@gmail.com>
2025-10-28 16:45:22 +0100

    add event-id

--- app/option/models.py
+++ app/option/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
+from app.event.models import Event
+
 
 class Option(models.Model):
     name = models.CharField(max_length=255)
+    event_id = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='options')
     value = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
